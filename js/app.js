d2b3ff90902287443b7d2796db6a232d63a64597
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-10 13:26:19 +0200

    second commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+// Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBBl8gLHFNd0Y_lmVmS_xS2_lTLC65DZcw",
+//   authDomain: "jurney-gym.firebaseapp.com",
+//   databaseURL: "https://jurney-gym.firebaseio.com",
+//   projectId: "jurney-gym",
+//   storageBucket: "jurney-gym.appspot.com",
+//   messagingSenderId: "643834081109",
+//   appId: "1:643834081109:web:4af758f73901ed7bdb89dc",
+//   measurementId: "G-493S5KY1XJ"
+// };
+// // Initialize Firebase
+// var db = firebase.initializeApp(firebaseConfig);
+// var ejercicio = db.collection('sesion').doc('ejercicio');
+
+const botones = document.querySelector('#botones');
+const nomrbreUsuario  = document.querySelector('#nombreUsuario');
+
+
+
+console.log('hola')
