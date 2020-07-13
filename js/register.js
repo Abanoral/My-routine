5b2b2f79143b423c94204381e766c0b00c40e4cf
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-13 18:08:12 +0200

    class register and user

--- js/register.js
+++ js/register.js
@@ -1,61 +1,57 @@
-const buttons = document.querySelector("#navButtons");
-const navUserName = document.querySelector("#navUserName");
-const header = document.querySelector('header')
-const main = document.querySelector('#main-container');
-
-firebase.auth().onAuthStateChanged( user => {
-  if (user) {
-    console.log(user);
-    buttons.innerHTML = /*html*/ `
-      <button class="btn btn-outline-danger" id="btn-log-out">Cerrar sesion</button>
-    `
-    navUserName.innerHTML = user.displayName;
-    btnLogOutSession();
-    main.innerHTML = '';
-  } else {
-    console.log("No existe usuario");
-    navUserName.innerHTML = 'My routine';
-
-    buttons.innerHTML = /*html*/ `
-      <button class="btn btn-outline-success" id="btn-access">Acceder</button>
-    `;
-    main.innerHTML = /*html*/ `
-    <form action="">
-      <label for="userName">Username</label>
-      <input type="text" placeholder="Userame" id="username" >
-      <label for="email">Email</label>
-      <input type="email" placeholder="Email" id="email" >
-      <label for="password">Password</label>
-      <input type="password" placeholder="Password" id="password" >
-      <label for="confirm-password">Confirm password</label>
-      <input type="password" placeholder="Confirm password" id="confirm-password" >
-      <dir id="container-button-signup">
-        <button type="submit" id="signup-button">Signup</button>
-      </dir>
-    </form>
-    `
-    header.appendChild(main);
-    initSession();
-    navUserName.innerHTML = 'My routine';
+class Register {
+  constructor(){
+    this.userNameInput = document.querySelector('#username')
+    this.emailInput = document.querySelector('#email')
+    this.passwordInput = document.querySelector('#password')
+    this.confirmPasswordInput = document.querySelector('#confirm-password')
+    
+    this.signupButton = document.querySelector('#signup-button');
+    this.errorsWrapper = document.querySelector('.message-container');
   }
-});
 
-const initSession = () => {
-  const btnAccess = document.querySelector('#btn-access');
-  btnAccess.addEventListener('click', async() => {
-    try {
-      const provider = new firebase.auth.GoogleAuthProvider(); // se configura el provider
-      console.log(provider)
-      await firebase.auth().signInWithPopup(provider) // proporcionamos el provider
-    } catch (error) {
-      console.log(error)
-    }
-  })
-}
+  handleEmailInput = event => {
+    const email = event.target.value;
+    console.log('email', email)
+  }
 
-const btnLogOutSession = () => {
-  const btnLogOut = document.querySelector('#btn-log-out');
-  btnLogOut.addEventListener('click', () => {
-    firebase.auth().signOut()
-  })
+  handlePasswordInput = event => {
+    const password = event.target.value;
+    console.log('password', password)
+    
+  }
+  
+  handleRepeatInput = event => {
+    const repeatPassword = event.target.value;
+    console.log('repeatPassword', repeatPassword)
+  }
+  
+  saveData = event => {
+    const userNameInput = this.userNameInput.value;
+    const emailInput = this.emailInput.value;
+    const passwordInput = this.passwordInput.value;
+    const confirmPasswordInput = this.confirmPasswordInput.value;
+
+    const newUser = new User(name, description, reps, sets, video, image);
+
+    // se debe vaciar el form
+    this.userNameInput.value = '';
+    this.emailInput.value = '';
+    this.passwordInput.value = '';
+    this.confirmPasswordInput.value = '';
+
+  }
+
+  addListeners = () => {
+    this.emailInput.addEventListener('input', this.handlePasswordInput);
+    this.passwordInput.addEventListener('input', this.handlePasswordInput);
+    this.confirmPasswordInput.addEventListener('input', this.handleRepeatInput);
+
+    this.signupButton.addEventListener('click', this.saveData);
+  }
+
+
 }
+// nueva instancia
+const signUp = new Register();
+// se cargan todas los event listeners en cuanto se cargue la web
+window.addEventListener('load', signUp.addListeners)
