f5a7a26387d52140a476fc339ff88dff03f993d9
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-15 14:29:04 +0200

    getting data from url

--- js/home.js
+++ js/home.js
@@ -4,21 +4,22 @@ class Home {
     
   }
 
-  userNameHome = () => {
+  sessionToUrl = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('session');
   }
 
   showSessions = async () => {
     const sessionsData = await getData();
     const sessions = Object.entries(sessionsData).map( // tiene dos valores nombre de la session y sus ejercicios
       session => session);
-    
-      
-      
       
       for(let i = 0; i <= sessions.length; i++){
         //cambiamos el titulo
-      const listSessionContainer = document.createElement('div');
-      listSessionContainer.setAttribute('class', 'listSessionContainer');
+      const listSessionContainer = document.createElement('a');
+      listSessionContainer.setAttribute('id', 'session-button');
+      listSessionContainer.setAttribute('href', `./exercise.html?session=${sessions[i][0]}`);
 
       const listSessionTitle = document.createElement('h2');
       listSessionTitle.innerHTML = sessions[i][0];
@@ -27,7 +28,6 @@ class Home {
       listSessionContainer.appendChild(listSessionTitle);
     }
 
-    console.log(sessions)
   }
 }
 
